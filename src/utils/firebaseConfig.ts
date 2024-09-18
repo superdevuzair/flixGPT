9d0044240f468879b05e1cf1db2ccb880f8ef83f
superdevuzair <uzair.ahmed170295@gmail.com>
2024-09-18 10:16:23 +0500

    firebase configuration

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFfBLlJ6hEMYdEmds_ljEA58JsRqRilXs",
+  authDomain: "flixgpt-8565b.firebaseapp.com",
+  projectId: "flixgpt-8565b",
+  storageBucket: "flixgpt-8565b.appspot.com",
+  messagingSenderId: "784695851076",
+  appId: "1:784695851076:web:34bf811f43b05bbf006411",
+  measurementId: "G-KKNHMSWGBN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
